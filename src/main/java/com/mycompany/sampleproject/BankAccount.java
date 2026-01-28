ae7d6690870bc2b8d23452516b3df99e4cab862c
Jimmy <gxs0017@gmail.com>
2026-01-28 14:08:20 -0500

    Created a Branch Called Guransh's Code [ Master ]

---
+++ src/main/java/com/mycompany/sampleproject/BankAccount.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sampleproject;
+
+/**
+ *
+ * @author Jimmy
+ */
+
+public class BankAccount {
+  
+    public String accountHolderName;
+    public int totalBalance;
+
+   
+    
+
+ 
+    public String getAccountSummary() {
+        return " Your Account Summary is -110";
+    }
+    
+    
+    public String getBalance() {
+        return "You are owed a balance of $110";
+    }
+    
+    // Nested Class for additional testing purposes
+    // Added after Video Making
+    public class NestedAccount {
+    
+    public String NestedAccName;
+    public String NestedAccBalance;
+    
+    }
+}
---
+++ src/test/java/com/mycompany/sampleproject/BankAccountTest.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package com.mycompany.sampleproject;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author Jimmy
+ */
+public class BankAccountTest {
+    
+    public BankAccountTest() {
+    }
+    
+    
+    /**
+     * Test of getAccountSummary method, of class BankAccount.
+     */
+    @Test
+    public void testGetAccountSummary() {
+        System.out.println("getAccountSummary");
+        BankAccount instance = new BankAccount();
+        assertEquals(" Your Account Summary is -110", instance.getAccountSummary());
+        
+    }
+
+    /**
+     * Test of getBalance method, of class BankAccount.
+     */
+    @Test
+    public void testGetBalance() {
+        System.out.println("getBalance");
+        BankAccount instance = new BankAccount();
+        assertEquals("Your Balance is $200", instance.getBalance());
+        
+    }
+    
+}
